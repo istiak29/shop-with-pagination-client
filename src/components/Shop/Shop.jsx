e0660f8c5ff17f1c5252d43e209533b47d3369c2
Istiak Islam <tistiak@outlook.com>
2024-01-18 05:53:56 +0600

    server link add

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,37 +3,74 @@ import { addToDb, deleteShoppingCart, getShoppingCart } from '../../utilities/fa
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
-import { Link } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState([])
+
+    const carts = useLoaderData()
+
+    const [cart, setCart] = useState(carts);
+
+
+    // pagination state
+    const [itemsPerPage, setItemsPerPage] = useState(10)
+
+    // current page state
+    const [currentPage, setCurrentPage] = useState(0);
+
+    // from the loader get all number of counted products
+    // const { count } = useLoaderData();
+
+    const [count, setCount] = useState(0)
+
+    // const itemsPerPage = 10;
+    const numberOfPages = Math.ceil(count / itemsPerPage);
+
+    // a way to find pages number. using for loop
+    // const pages = [];
+    // for (let i = 0; i < numberOfPages; i++){
+    //     pages.push(i)
+    // }
+    // console.log(pages)
+
+    // using array
+    const pages = [...Array(numberOfPages).keys()]
+    console.log("Total pages:", pages)
+
+
 
     useEffect(() => {
-        fetch('http://localhost:5000/products')
+        fetch('https://ema-john-pagination-server-starter-kyntkwjf2-triistiak-gmailcom.vercel.app/productCount')
+            .then(res => res.json())
+        .then(data => setCount(data.count))
+    }, [])
+
+    useEffect(() => {
+        fetch(`https://ema-john-pagination-server-starter-kyntkwjf2-triistiak-gmailcom.vercel.app/products?page=${currentPage}&size=${itemsPerPage}`)
             .then(res => res.json())
             .then(data => setProducts(data))
-    }, []);
+    }, [currentPage, itemsPerPage]);
 
-    useEffect(() => {
-        const storedCart = getShoppingCart();
-        const savedCart = [];
-        // step 1: get id of the addedProduct
-        for (const id in storedCart) {
-            // step 2: get product from products state by using id
-            const addedProduct = products.find(product => product._id === id)
-            if (addedProduct) {
-                // step 3: add quantity
-                const quantity = storedCart[id];
-                addedProduct.quantity = quantity;
-                // step 4: add the added product to the saved cart
-                savedCart.push(addedProduct);
-            }
-            // console.log('added Product', addedProduct)
-        }
-        // step 5: set the cart
-        setCart(savedCart);
-    }, [products])
+    // useEffect(() => {
+    //     const storedCart = getShoppingCart();
+    //     const savedCart = [];
+    //     // step 1: get id of the addedProduct
+    //     for (const id in storedCart) {
+    //         // step 2: get product from products state by using id
+    //         const addedProduct = products.find(product => product._id === id)
+    //         if (addedProduct) {
+    //             // step 3: add quantity
+    //             const quantity = storedCart[id];
+    //             addedProduct.quantity = quantity;
+    //             // step 4: add the added product to the saved cart
+    //             savedCart.push(addedProduct);
+    //         }
+    //         // console.log('added Product', addedProduct)
+    //     }
+    //     // step 5: set the cart
+    //     setCart(savedCart);
+    // }, [products])
 
     const handleAddToCart = (product) => {
         // cart.push(product); '
@@ -61,6 +98,31 @@ const Shop = () => {
         deleteShoppingCart();
     }
 
+    // product per page handler
+    const handleChangeItemPerPage = event => {
+        const perPageValue = parseInt(event.target.value)
+        console.log('page:', perPageValue);
+        setItemsPerPage(perPageValue);
+        // console.log('Items per page:', itemsPerPage)
+
+        // if we select number of page from selector then the current page should be 0. So we need to set current page 0 while clicking the selector.
+        setCurrentPage(0);
+    }
+
+    // 
+    const handlePrevButton = () => {
+        if (currentPage > 0) {
+            setCurrentPage(currentPage - 1);
+        }
+    }
+    
+    const handleNextButton = () => {
+        if (currentPage <  pages.length - 1) {
+            setCurrentPage(currentPage + 1);
+            console.log('current Page:',currentPage)
+        }
+    }
+
     return (
         <div className='shop-container'>
             <div className="products-container">
@@ -82,6 +144,36 @@ const Shop = () => {
                     </Link>
                 </Cart>
             </div>
+
+            <div className='pagination'>
+
+                <h3>current page: {currentPage}</h3>
+                
+                <button onClick={handlePrevButton}>Prev</button>
+
+                {
+                    pages.map(page => <button
+                        className={currentPage === page ? 'selected' : undefined}
+                        onClick={() => setCurrentPage(page)}
+                        key={page}
+                    >{page}</button>)
+                }
+
+                <button onClick={handleNextButton}>Next</button>
+
+
+                {/* how many page should we display in a page */}
+                <div>
+                    <select name="" id="" value={itemsPerPage} onChange={handleChangeItemPerPage}>
+                        <option value="5">5</option>
+                        <option value="10">10</option>
+                        <option value="20">20</option>
+                        <option value="50">50</option>
+                    </select>
+                </div>
+
+            </div>
+
         </div>
     );
 };
